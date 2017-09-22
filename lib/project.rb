f23f958c4f1083bdd27c56e056cdc227d2360039
Dan Nollette <nollette.dan@gmail.com>
2017-09-22 10:04:25 -0700

    passes first project spec '#title'

--- lib/project.rb
+++ lib/project.rb
@@ -1,6 +1,7 @@
 class Project
-#
-#   def initialize(attributes)
-#     @id =
-#     @
+  attr_reader :title
+  def initialize(attributes)
+    @id = attributes.fetch(:id)
+    @title = attributes.fetch(:title)
   end
+end
