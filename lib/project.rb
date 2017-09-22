719af269a48d2ec6270e0fe40bf89c64e914ecc9
Dan Nollette <nollette.dan@gmail.com>
2017-09-22 10:35:57 -0700

    passes fourth, fifth, and second half of second project spec '#save' and '.all'

--- lib/project.rb
+++ lib/project.rb
@@ -8,4 +8,20 @@ class Project
   def == (other_project)
     self.title == other_project.title
   end
+
+  def save
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result
+  end
+
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each do |project|
+      id = project.fetch("id")
+      title = project.fetch("title")
+      projects.push(Project.new({id: id.to_i, title: title}))
+    end
+    projects
+  end
 end
