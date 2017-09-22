51472278d7d6f70b9d045edad345e49de81ea83b
Dan Nollette <nollette.dan@gmail.com>
2017-09-22 09:10:47 -0700

    Add tests

---
+++ app.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+require 'pg'
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/volunteer')
+require('./lib/project')
+
+
+DB = PG.connect({:dbname => 'volunteer_tracker'})
